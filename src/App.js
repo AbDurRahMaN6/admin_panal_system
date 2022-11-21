274ddd6eb395d212bbf27d2f24ad0fa12667152f
Abdurrahman <abdurrahmanms5258@gmail.com>
2022-11-21 16:31:13 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import rootRoutes from './components/admin/rootRoutes';
 import NoMatch from './components/nomatch';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 // import PrivateRoute from './components/auth/PrivateRoute';
-import { NotificationContainer } from "react-notifications";
+// import { NotificationContainer } from "react-notifications";
 import "react-notifications/lib/notifications.css";
 export default class App extends Component {
 	render() {
